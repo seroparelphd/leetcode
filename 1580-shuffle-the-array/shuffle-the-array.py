db6c64d5bb8cf55c5cd775de7721bd3d38eaf2a9
Sero Toriano Parel, PhD <43653950+seroparelphd@users.noreply.github.com>
2025-12-01 19:20:21 -0500

    Time: 52 ms (87.87%) | Memory: 17.9 MB (86.79%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.py
+++ 1580-shuffle-the-array/shuffle-the-array.py
@@ -1,12 +1,12 @@
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
-        nums_a = nums[0:n]
+        left = nums[0:n]
         nums_b = nums[n:2*n]
         nums2 = ["NA"] * (2*n)
         j = 0
 
         for i in range(0,2*n,2):
-            nums2[i] = nums_a[j]
+            nums2[i] = left[j]
             nums2[i+1] = nums_b[j]
             j += 1
 
