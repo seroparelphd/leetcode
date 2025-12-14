d3de20b3e0b4459d8c9ee1a7f36b4ad2a20b2015
Sero Toriano Parel, PhD <43653950+seroparelphd@users.noreply.github.com>
2025-12-14 13:33:10 -0500

    Time: 55 ms (28.90%) | Memory: 26.8 MB (88.86%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.py
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.py
@@ -4,5 +4,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        expected = list(range(1,len(nums)+1))
-        return list(set(nums) ^ set(expected))
+        # expected = list(range(1,len(nums)+1))
+        # return list(set(nums) ^ set(expected))
+
+        for n in nums:
+            i = abs(n) - 1
+            nums[i] = -1 * abs(nums[i])
+
+        res = []
+        for i, n in enumerate(nums):
+            if n > 0:
+                res.append(i + 1)
+        return res
