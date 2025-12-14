d6b20b3720fb020697c3e6de011dfcd60f2b1582
Sero Toriano Parel, PhD <43653950+seroparelphd@users.noreply.github.com>
2025-12-14 13:34:18 -0500

    Time: 37 ms (53.67%) | Memory: 31 MB (5.04%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.py
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.py
@@ -4,15 +4,8 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        # expected = list(range(1,len(nums)+1))
-        # return list(set(nums) ^ set(expected))
+        # What are all numbers 1 to n expected
+        expected = list(range(1,len(nums)+1))
 
-        for n in nums:
-            i = abs(n) - 1
-            nums[i] = -1 * abs(nums[i])
-
-        res = []
-        for i, n in enumerate(nums):
-            if n > 0:
-                res.append(i + 1)
-        return res
+        # Which numbers are not in the intersection
+        return list(set(nums) ^ set(expected))
