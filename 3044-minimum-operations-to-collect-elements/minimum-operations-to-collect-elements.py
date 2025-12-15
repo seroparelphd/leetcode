b71230e0e697ffa48fba6e0df15745d8799ba899
Sero Toriano Parel, PhD <43653950+seroparelphd@users.noreply.github.com>
2025-12-14 21:17:40 -0500

    Time: 8 ms (9.43%) | Memory: 12.3 MB (81.13%) - LeetSync

--- 3044-minimum-operations-to-collect-elements/minimum-operations-to-collect-elements.py
+++ 3044-minimum-operations-to-collect-elements/minimum-operations-to-collect-elements.py
@@ -10,8 +10,11 @@ class Solution(object):
         target = [x for x in range(1,k+1)]
 
         for i in range(0,len(nums)+1):
+
+            # Check if target is achieved
             if set(target).issubset(set(collection)):
                 return ops
             
+            # If not, keep collecting and count operation
             collection.append(nums.pop())
             ops += 1
