5af8a4d17a1809a60849450480d7c92c35e0188f
Sero Toriano Parel, PhD <43653950+seroparelphd@users.noreply.github.com>
2025-12-18 20:52:34 -0500

    Time: 23 ms (10.76%) | Memory: 12.6 MB (12.66%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.py
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -5,15 +5,16 @@ class Solution(object):
         :type word2: str
         :rtype: str
         """
-        word3 = ""
+        word3 = []
         word_min = min(len(word1),len(word2))
 
         for i in range(0,word_min):
-            word3 += word1[i] + word2[i]
+            word3.append(word1[i])
+            word3.append(word2[i])
 
         if len(word1) > len(word2):
-            word3 += word1[(i+1):]
+            word3.append(word1[word_min:])
         else:
-            word3 += word2[(i+1):]
+            word3.append(word2[word_min:])
 
-        return word3
+        return "".join(word3)
