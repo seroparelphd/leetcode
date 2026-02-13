c38284cd70ac285670332b9d16eb10163a01f8ee
Sero Toriano Parel, PhD <43653950+seroparelphd@users.noreply.github.com>
2026-02-12 23:12:23 -0500

    Time: 5 ms (65.26%) | Memory: 21.1 MB (65.59%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,25 +1,20 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        counts = {}
-        current_max = 0
-        result = 0
-        for num in nums:
-            counts[num] = counts.get(num, 0) + 1
-            # current_max = max()
-            if counts[num] > current_max:
-                result = num
-            current_max = max(counts[num], current_max)
-        return result
 
-
-
-        # count = 0
+        # counts = {}
+        # current_max = 0
         # result = 0
         # for num in nums:
-        #     if count == 0:
+        #     counts[num] = counts.get(num, 0) + 1
+        #     # current_max = max()
+        #     if counts[num] > current_max:
         #         result = num
-        #     if num == result:
-        #         count += 1
-        #     else:
-        #         count -= 1
+        #     current_max = max(counts[num], current_max)
         # return result
+
+        candidate, count = 0, 0
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += (1 if num == candidate else -1)
+        return candidate
