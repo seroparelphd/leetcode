340764b772e0bf4705869202cfe83450b25c7c3e
Sero Toriano Parel, PhD <43653950+seroparelphd@users.noreply.github.com>
2026-02-13 13:30:26 -0500

    Time: 6 ms (61.40%) | Memory: 21.3 MB (14.80%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,5 +1,14 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
+        candidate = 0
+        count = 0
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += (1 if num == candidate else -1) 
+        return candidate
+
+
 
         # counts = {}
         # current_max = 0
@@ -12,9 +21,9 @@ class Solution:
         #     current_max = max(counts[num], current_max)
         # return result
 
-        candidate, count = 0, 0
-        for num in nums:
-            if count == 0:
-                candidate = num
-            count += (1 if num == candidate else -1)
-        return candidate
+        # candidate, count = 0, 0
+        # for num in nums:
+        #     if count == 0:
+        #         candidate = num
+        #     count += (1 if num == candidate else -1)
+        # return candidate
