2ba2fbe3429005aeb56c842f0b5d377e9b1e25e8
Sero Toriano Parel, PhD <43653950+seroparelphd@users.noreply.github.com>
2026-02-20 22:38:21 -0500

    Time: 3 ms (43.32%) | Memory: 19.5 MB (17.89%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
@@ -9,12 +9,30 @@ class Solution:
         #         nums2.remove(num)
         # return res
 
-        counts = Counter(nums1)
-        # print(counts)
-        res = []
-        for num in nums2:
-            if counts[num] > 0:
-                res.append(num)
-                counts[num] -= 1
+        # counts = Counter(nums1)
+        # res = []
+        # for num in nums2:
+        #     if counts[num] > 0:
+        #         res.append(num)
+        #         counts[num] -= 1
+        # return res
+
+        nums1.sort()  # [4, 5, 9]
+                      #     i
+        nums2.sort()  # [4, 4, 8, 9, 9]
+                      #        j
+        i, j = 0, 0  
+        res = []  # [4]
+
+        # i = 1, j = 2
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] == nums2[j]:
+                res.append(nums1[i])
+                i += 1
+                j += 1
+            elif nums1[i] < nums2[j]:
+                i += 1
+            else: 
+                j += 1
         return res
 
