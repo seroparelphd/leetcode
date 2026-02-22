d2656cba5708d12238296babe1bb5e8a9adb9398
Sero Toriano Parel, PhD <43653950+seroparelphd@users.noreply.github.com>
2026-02-22 17:36:26 -0500

    Time: 132 ms (90.50%) | Memory: 50 MB (26.19%) - LeetSync

--- 1249-snapshot-array/snapshot-array.py
+++ 1249-snapshot-array/snapshot-array.py
@@ -3,7 +3,7 @@ class SnapshotArray:
     def __init__(self, length: int):
         self.snap_n = 0
         self.history = [[[-1, 0]] for _ in range(length)]
-        
+
     def set(self, index: int, val: int) -> None:
         self.history[index].append([self.snap_n, val])
 
@@ -14,9 +14,6 @@ class SnapshotArray:
     def get(self, index: int, snap_id: int) -> int:
         idx = bisect_right(self.history[index], snap_id, key=lambda x: x[0])
         return self.history[index][idx - 1][1]
-        # return bisect_right(self.history[index], snap_id)
-
-        # print(f"index = {index} | snap_id = {snap_id} | self.history = {self.history} | self.history[snap_id][index] = {self.history[snap_id][index]}")
 
 # Your SnapshotArray object will be instantiated and called as such:
 # obj = SnapshotArray(length)
