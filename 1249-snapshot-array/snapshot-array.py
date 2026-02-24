bdbc6d8724148130b95ab403ff76069cea4c8de6
Sero Toriano Parel, PhD <43653950+seroparelphd@users.noreply.github.com>
2026-02-23 20:09:47 -0500

    Time: 152 ms (80.02%) | Memory: 50.2 MB (23.05%) - LeetSync

--- 1249-snapshot-array/snapshot-array.py
+++ 1249-snapshot-array/snapshot-array.py
@@ -1,31 +1,18 @@
 class SnapshotArray:
 
     def __init__(self, length: int):
-        '''
-        initializes an array-like data structure with the given length. 
-        Initially, each element equals 0—BUT HOW?!
-        '''
-        self.id = 0
+        self.snap_id = 0
         self.history = [[[-1, 0]] for _ in range(length)]
 
     def set(self, index: int, val: int) -> None:
-        '''
-        sets the element at the given index to be equal to val
-        '''
-        self.history[index].append([self.id, val])
+        self.history[index].append([self.snap_id, val])
 
     def snap(self) -> int:
-        '''
-        takes a snapshot of the array and returns the snap_id: the total number of times we called snap() minus 1
-        '''
-        self.id += 1
-        return self.id - 1
+        self.snap_id += 1
+        return self.snap_id - 1
 
     def get(self, index: int, snap_id: int) -> int:
-        '''
-        returns the value at the given index, at the time we took the snapshot with the given snap_id
-        '''
-        ix = bisect_right(self.history[index], snap_id, key = lambda x: x[0])
+        ix = bisect_right(self.history[index], [snap_id, float('inf')])
         return self.history[index][ix - 1][1]
 
 # Your SnapshotArray object will be instantiated and called as such:
