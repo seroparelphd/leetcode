af3ca5fb5cbdadf3ca8a9b85c481442d4bed70b1
Sero Toriano Parel, PhD <43653950+seroparelphd@users.noreply.github.com>
2026-03-12 19:05:19 -0400

    Time: 36 ms (95.20%) | Memory: 19.3 MB (53.00%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.py
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -1,17 +1,24 @@
+# class Solution:
+#     def mergeAlternately(self, word1: str, word2: str) -> str:
+#         pt1, pt2 = 0, 0
+#         merged = []
+        
+#         while pt1 < len(word1) and pt2 < len(word2):
+#             merged.append(word1[pt1])
+#             merged.append(word2[pt2])
+#             # print(merged)
+#             pt1 += 1
+#             pt2 += 1
+        
+#         merged.append(word1[pt1:])
+#         merged.append(word2[pt2:])
+#         merged = "".join(merged)
+#         return merged
+
+from itertools import zip_longest
+
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        pt1, pt2 = 0, 0
-        merged = []
-        
-        while pt1 < len(word1) and pt2 < len(word2):
-            merged.append(word1[pt1])
-            merged.append(word2[pt2])
-            # print(merged)
-            pt1 += 1
-            pt2 += 1
-        
-        merged.append(word1[pt1:])
-        merged.append(word2[pt2:])
-        merged = "".join(merged)
-        return merged
-
+        # zip_longest pairs characters and uses fillvalue='' for the shorter string
+        # Then we join them all in one shot
+        return "".join(a + b for a, b in zip_longest(word1, word2, fillvalue=''))
