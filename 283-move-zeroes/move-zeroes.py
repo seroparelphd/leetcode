558a3ce6a443dbb8e50be3adbd93d0aca612a8e6
Sero Toriano Parel, PhD <43653950+seroparelphd@users.noreply.github.com>
2026-03-17 20:14:54 -0400

    Time: 7 ms (42.38%) | Memory: 20.6 MB (20.13%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -3,28 +3,19 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # nums[-1] = nums.pop(0)
-        # print(nums)
         l = 0
-        # while l < r:
-        #     if nums[l] == 0:
-        #         nums.append(nums.pop(l))
         for r in range(len(nums)):
             if nums[r] != 0:
                 nums[l], nums[r] = nums[r], nums[l]
                 l += 1
 
-                
-
-
-# nums = [0,1,0,3,12]
-
 # nums = [0,1,0,3,12]
 # nums = [1,0,0,3,12]
 # nums = [1,3,0,0,12]
+# nums = [1,3,12,0,0]
 #             l
-#                 r
-# l = 1
+#                  r
+# l = 2
 # r = 3
 # nums[l] = 0
 # nums[r] = 3
