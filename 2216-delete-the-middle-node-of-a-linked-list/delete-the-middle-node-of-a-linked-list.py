e963a4cd8611d1e82cc7d3a03d9e4a12c96bd948
Sero Toriano Parel, PhD <43653950+seroparelphd@users.noreply.github.com>
2026-04-02 18:41:58 -0400

    Time: 67 ms (94.12%) | Memory: 62.3 MB (84.85%) - LeetSync

--- 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.py
+++ 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.py
@@ -5,23 +5,47 @@
 #         self.next = next
 class Solution:
     def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # if not head or not head.next:
+        #     return None
+
+        # # Get length
+        # length = 0
+        # current = head
+        # while current:
+        #     length += 1
+        #     current = current.next
+        # # print(length)
+
+        # # Find middle and skip it
+        # # middle = (length - 1) // 2
+        # middle = (length - 2) // 2
+        # # print(middle)
+        # curr = head
+        # for i in range(middle):
+        #     curr = curr.next
+        # curr.next = curr.next.next
+        
+        # return head
+
+# Time: O(2n) -> O(n)
+# Space O(4) -> O(1)
+
+
+        # Handle the 1-node case
         if not head or not head.next:
             return None
-        # Get length
-        length = 0
-        current = head
-        while current:
-            length += 1
-            current = current.next
-        # print(length)
 
-        # Find middle and skip it
-        # middle = (length - 1) // 2
-        middle = (length - 2) // 2
-        # print(middle)
-        curr = head
-        for i in range(middle):
-            curr = curr.next
-        curr.next = curr.next.next
+        # Start slow at head, fast two steps ahead
+        slow = head
+        fast = head.next.next
+
+        # While fast can move two steps
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # After the loop, slow is perfectly positioned 
+        # right before the middle node.
+        slow.next = slow.next.next
         
         return head
