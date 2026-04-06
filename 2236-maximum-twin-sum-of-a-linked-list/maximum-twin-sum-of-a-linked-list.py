844f968bb9c71326451bd9b90af2f71c99a7cd49
Sero Toriano Parel, PhD <43653950+seroparelphd@users.noreply.github.com>
2026-04-06 18:24:25 -0400

    Time: 47 ms (92.78%) | Memory: 63 MB (23.15%) - LeetSync

--- 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.py
+++ 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.py
@@ -10,17 +10,12 @@ class Solution:
         while current:
             head_list.append(current.val)
             current = current.next
-        # print(head_list)
         max_twin_sum = float(-inf)
         head_list_len = len(head_list)
         r = head_list_len - 1
-        # for l in range(head_list_len/2 - 1):
         l = 0
-        print(head_list[l])
         while l < r:
-            # print(head_list[l])
             max_twin_sum = max(max_twin_sum, head_list[l] + head_list[r])
-            print(max_twin_sum)
             l += 1
             r -= 1
         return max_twin_sum
