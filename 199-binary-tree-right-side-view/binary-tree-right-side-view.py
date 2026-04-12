b9e9e249aba09b84abbfe61be18ce096c09e95ba
Sero Toriano Parel, PhD <43653950+seroparelphd@users.noreply.github.com>
2026-04-12 15:30:54 -0400

    Time: 2 ms (10.52%) | Memory: 19.5 MB (17.11%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.py
@@ -10,18 +10,21 @@ from collections import deque
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
         result = []
-        queue = deque([root])
+        queue = deque([root])  # For popleft
         print(queue)
         while queue:
-            queue_len = len(queue)
-            right_node = None  # Track last valid node seen at given level
+            queue_len = len(queue)  # Process current depth's nodes
+            right_node = None  # Track right-most node seen at given depth
             for i in range(queue_len):
                 node = queue.popleft()
                 if node:
-                    right_node = node  # Keep updating under end of level
-                    queue.append(node.left)
-                    queue.append(node.right)
-            if right_node:
+                    right_node = node  # Keep updating under end of depth/level; assign right-most
+                    queue.append(node.left)  # Add left child to process at next depth
+                    queue.append(node.right)  # Add right
+            if right_node:  # If not empty, record val of right-most node
                 result.append(right_node.val)
 
         return result
+
+# Time O(n)
+# Space O(w), where w = max width of tree
