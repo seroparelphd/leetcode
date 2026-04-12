3039d622bf945a846370350671938d55404e49f1
Sero Toriano Parel, PhD <43653950+seroparelphd@users.noreply.github.com>
2026-04-12 15:38:18 -0400

    Time: 0 ms (100.00%) | Memory: 19.4 MB (44.12%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.py
@@ -4,24 +4,21 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
 from collections import deque
-
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
         result = []
-        queue = deque([root])  # For popleft
-        print(queue)
+        queue = deque([root])                   # For popleft
         while queue:
-            queue_len = len(queue)  # Process current depth's nodes
-            right_node = None  # Track right-most node seen at given depth
+            queue_len = len(queue)              # Process current depth's nodes
+            right_node = None                   # Track right-most node seen at given depth
             for i in range(queue_len):
                 node = queue.popleft()
                 if node:
-                    right_node = node  # Keep updating under end of depth/level; assign right-most
-                    queue.append(node.left)  # Add left child to process at next depth
-                    queue.append(node.right)  # Add right
-            if right_node:  # If not empty, record val of right-most node
+                    right_node = node           # Keep updating under end of depth/level; assign right-most
+                    queue.append(node.left)     # Add left child to process at next depth
+                    queue.append(node.right)    # Add right
+            if right_node:                      # If not empty, record val of right-most node
                 result.append(right_node.val)
 
         return result
