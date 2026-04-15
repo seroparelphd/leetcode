9fd2326024b1d5e82dbcf2c241d981b801afbe59
Sero Toriano Parel, PhD <43653950+seroparelphd@users.noreply.github.com>
2026-04-15 18:44:49 -0400

    Time: 0 ms (100.00%) | Memory: 21 MB (48.54%) - LeetSync

--- 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.py
+++ 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.py
@@ -1,26 +1,22 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def searchBST(self, root, val):
-        """
-        :type root: Optional[TreeNode]
-        :type val: int
-        :rtype: Optional[TreeNode]
-        """
-        if not root or root.val == val:
-            return root
-        elif val < root.val:
-            return self.searchBST(root.left,val)
-        else:
-            return self.searchBST(root.right,val)
-
-        # while root and root.val != val:
-        #     if val < root.val:
-        #         root = root.left 
-        #     else: 
-        #         root.right
-        # return root
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        # stack = []
+        while root:
+            if root.val == val:
+                # print(f"root.val = {root.val}")
+                # stack.append(root.val)
+                return root
+            elif root.val > val:
+                # stack.append(root.val)
+                root = root.left
+            elif root.val < val:
+                # stack.append(root.val)
+                root = root.right
+            else:
+                return []
