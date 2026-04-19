1c2f2fe0e7fc1a98850c3f2e9ead7e7537ae2ba5
Sero Toriano Parel, PhD <43653950+seroparelphd@users.noreply.github.com>
2026-04-19 15:00:07 -0400

    Time: 37 ms (5.57%) | Memory: 21.5 MB (30.92%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -3,16 +3,15 @@ class Solution:
 
         def dfs(city):
             for other_city in range(len(isConnected)):
-                if isConnected[city][other_city] == 1 and other_city not in visited:
-                    visited.add(other_city)
+                if isConnected[city][other_city] == 1:
+                    isConnected[city][other_city] = 0
+                    isConnected[other_city][city] = 0
                     dfs(other_city)
 
         num_provinces = 0
-        visited = set()
         for city in range(len(isConnected)):
-            if city not in visited:
+            if isConnected[city][city] == 1:
                 num_provinces += 1
-                visited.add(city)
                 dfs(city)
 
         return num_provinces
