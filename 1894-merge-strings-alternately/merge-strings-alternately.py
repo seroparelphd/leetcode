cab417d72fe0bfaf28fd091e4beaed541d928d4f
Sero Toriano Parel, PhD <43653950+seroparelphd@users.noreply.github.com>
2026-05-11 14:53:49 -0400

    Time: 38 ms (91.11%) | Memory: 19.2 MB (88.15%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.py
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -1,25 +1,12 @@
-# class Solution:
-#     def mergeAlternately(self, word1: str, word2: str) -> str:
-#         pt1, pt2 = 0, 0
-#         merged = []
-        
-#         while pt1 < len(word1) and pt2 < len(word2):
-#             merged.append(word1[pt1])
-#             merged.append(word2[pt2])
-#             # print(merged)
-#             pt1 += 1
-#             pt2 += 1
-        
-#         merged.append(word1[pt1:])
-#         merged.append(word2[pt2:])
-#         merged = "".join(merged)
-#         return merged
-
-from itertools import zip_longest
-
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        # 1. zip_longest creates an iterator in C (no Python loop overhead)
-        # 2. The generator expression (a + b...) stays in a lower-level execution layer
-        # 3. "".join() is a single C-call that pre-calculates memory needs
-        return "".join(a + b for a, b in zip_longest(word1, word2, fillvalue=''))
+        res = ""
+        shortest = min(len(word1), len(word2))
+        print(shortest)
+        for i in range(shortest):
+            print(word1[i])
+            res = res + word1[i] + word2[i]
+            print(res)
+        res = res + word1[shortest:]
+        res = res + word2[shortest:]
+        return res
