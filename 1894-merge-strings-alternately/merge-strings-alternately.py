3069ff720333c2af9bd88757d3c6238bd0bdd407
Sero Toriano Parel, PhD <43653950+seroparelphd@users.noreply.github.com>
2026-05-11 14:54:07 -0400

    Time: 45 ms (56.34%) | Memory: 19.3 MB (55.93%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.py
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -2,11 +2,11 @@ class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
         res = ""
         shortest = min(len(word1), len(word2))
-        print(shortest)
+        # print(shortest)
         for i in range(shortest):
-            print(word1[i])
+            # print(word1[i])
             res = res + word1[i] + word2[i]
-            print(res)
+            # print(res)
         res = res + word1[shortest:]
         res = res + word2[shortest:]
         return res
