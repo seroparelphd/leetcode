7e123271ec35653c6f4d90f76213220b8fbe3194
Sero Toriano Parel, PhD <43653950+seroparelphd@users.noreply.github.com>
2026-06-16 19:56:42 -0400

    Time: 7 ms (72.36%) | Memory: 22.8 MB (42.71%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -2,18 +2,18 @@ class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         # print(sorted(intervals), intervals.sort())
         intervals = sorted(intervals)
-        print(intervals, intervals[0])
+        # print(intervals, intervals[0])
         
         merged = [intervals[0]]  # empty list
-        print(f"merged = {merged}")
-        print(len(intervals))
+        # print(f"merged = {merged}")
+        # print(len(intervals))
 
         for i in range(1, len(intervals)):
-            print(intervals[i])
-            print(intervals[i][0])
-            print(f"len(merged) = {len(merged)}")
+            # print(intervals[i])
+            # print(intervals[i][0])
+            # print(f"len(merged) = {len(merged)}")
             # print(f"merged[-1] = {merged[-1]}, merged[-1][1] = {merged[-1][1]}") #, i[0] = {i[0]}")
-            print(f"intervals[i][0] = {intervals[i][0]}")
+            # print(f"intervals[i][0] = {intervals[i][0]}")
             if merged[-1][1] >= intervals[i][0]:
                 # merged[-1][1] = intervals[i][1]
                 merged[-1][1] = max(merged[-1][1], intervals[i][1])
