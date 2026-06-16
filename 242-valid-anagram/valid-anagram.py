363b050a668a9edae25f16f4b44f7cb5f6f0223e
Sero Toriano Parel, PhD <43653950+seroparelphd@users.noreply.github.com>
2026-06-16 19:08:06 -0400

    Time: 19 ms (15.80%) | Memory: 19.7 MB (23.23%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,15 +1,8 @@
-# from collections import Counter
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # Check length. If different, return `False`
         if len(s) != len(t):
             return False
-        # Count frequency of char in String S and T
-        s_freq, t_freq = {}, {} # Counter()
-        for i in range(len(s)):
-            s_freq[s[i]] = 1 + s_freq.get(s[i], 0)
-            t_freq[t[i]] = 1 + t_freq.get(t[i], 0)
-
-        # Compare the two counts.
-        return s_freq == t_freq
-
+        if "".join(sorted(s)) == "".join(sorted(t)):
+            return True
+        else:
+            return False
