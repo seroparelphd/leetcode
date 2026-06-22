a21ae48071675c1dcea62970417391d09e8df1b5
Sero Toriano Parel, PhD <43653950+seroparelphd@users.noreply.github.com>
2026-06-22 18:52:05 -0400

    Time: 11 ms (83.53%) | Memory: 21.9 MB (79.79%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.py
@@ -0,0 +1,15 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hash_table = {}
+        for s in strs:
+            # print(f"s = {s}")
+            sorted_s = "".join(sorted(s))
+            # print(f"  sorted_s = {sorted_s}")
+            if sorted_s not in hash_table:
+                hash_table[sorted_s] = []
+            hash_table[sorted_s].append(s)
+        #     print(f"  hash_table = {hash_table}")
+        # print(f"final hash_table = {hash_table}")
+        # print(f"values hash_table = {list(hash_table.values())}")
+        return list(hash_table.values())
+            
