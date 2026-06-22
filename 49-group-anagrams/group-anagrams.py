60c34bb4abb649c14193ef88c0fdda31f70d469d
Sero Toriano Parel, PhD <43653950+seroparelphd@users.noreply.github.com>
2026-06-22 19:11:41 -0400

    Time: 11 ms (83.53%) | Memory: 22 MB (66.25%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -2,15 +2,15 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         hash_table = {}
         for s in strs:
-            print(f"s = {s}")
+            # print(f"s = {s}")
             sorted_s = "".join(sorted(s))
             # print(f"  sorted_s = {sorted_s}")
             if sorted_s not in hash_table:
                 hash_table[sorted_s] = []
             hash_table[sorted_s].append(s)
-        #     print(f"  hash_table = {hash_table}")
+            # print(f"  hash_table = {hash_table}")
         # print(f"final hash_table = {hash_table}")
-        print(f"values hash_table = {list(hash_table.values())}")
+        # print(f"values hash_table = {list(hash_table.values())}")
         return list(hash_table.values())
 
 # Time ~O(N)...~ O(N * K log K) where N is # of strings and K is max len of longest string
