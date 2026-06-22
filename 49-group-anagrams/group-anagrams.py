3ac3f1a0f98dc2c77ade59082a8d3eb183367a87
Sero Toriano Parel, PhD <43653950+seroparelphd@users.noreply.github.com>
2026-06-22 18:58:49 -0400

    Time: 12 ms (56.95%) | Memory: 21.9 MB (79.79%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -12,4 +12,6 @@ class Solution:
         # print(f"final hash_table = {hash_table}")
         # print(f"values hash_table = {list(hash_table.values())}")
         return list(hash_table.values())
-            
+
+# Time ~O(N)...~ O(N * K log K) where N is # of strings and K is max len of longest string
+# Space ~O(N)...~ O(N*K) 
