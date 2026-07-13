945eb81a87a35a95231fabc6d744727e0d68eb81
Sero Toriano Parel, PhD <43653950+seroparelphd@users.noreply.github.com>
2026-07-13 18:19:03 -0400

    Time: 0 ms (100.00%) | Memory: 19.3 MB (53.89%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -1,37 +1,22 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
-        # # Base case: n = 1 or 2
-        # if n <= 2:
-        #     # return n
-        #     if n != 0:
-        #         return 1
-        #     else:
-        #         return 0
-            
-        # # Recursive case: climbStairs(n) = climbStairs(n - 1) + climbStairs(n - 2)
-        # # return self.climbStairs(n - 1) + self.climbStairs(n - 2)
-        # return self.tribonacci(n - 1) + self.tribonacci(n -  2) + self.tribonacci(n - 3)        
-
+        
+        # Base cases
         if n == 0:
             return 0
         if n <= 2:
             return 1
 
-        a, b, c = 0, 1, 1
-        for _ in range(3, n + 1):  # range(3, 4 + 1)
-            # a = b
-            # b = c 
-            # c = a + b + c 
-            a, b, c = b, c, a + b + c
-        return c
+        # Function
+        trib1, trib2, trib3 = 0, 1, 1
+        for _ in range(3, n + 1):
+            trib1, trib2, trib3 = trib2, trib3, trib1 + trib2 + trib3
+        return trib3
 
-# Input: n = 4
-# Output: 4
-# Explanation:
-# T_3 = 0 + 1 + 1 = 2
-# T_4 = 1 + 1 + 2 = 4
-
-# a = 1
-# b = 2
-# c = 4
-# _ = 4
+        # if n <= 1:
+        #     return n
+        # cache = [0, 1, 1]
+        # if n in cache:
+        #     return cache[n]
+        # cache[n] = tribonacci(n - 1, cache) + tribonacci(n - 2, cache) + tribonacci(n - 3, cache)
+        # return cache[n]
